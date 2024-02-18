7af9b312741d1034d24310300b69caac243eabfc
jesus <jesumartt@hotmail.com>
2024-02-18 16:41:24 -0500

    instalacion del paquete express

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Este server se esta ejecutando...");
+//console.log("Este server se esta ejecutando...");
+const express = require("express"); //importacion del paquete express
+const app = new express(); //app es una constante de tipo obj instanciada
+app.listen(3000, ()=>{ //funcion flecha =>, reemplaza la palabra function 
+     console.log("Servidor corriendo desde el puerto 3000")
+});
