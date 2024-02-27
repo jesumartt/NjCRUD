f3abe29c11cb8211f57c27d04854c8deb97a39fd
jesus <jesumartt@hotmail.com>
2024-02-27 16:05:13 -0500

    Conexion a la BD

--- server.js
+++ server.js
@@ -2,24 +2,13 @@
 const express = require("express"); //importacion del paquete express
 const app = new express(); //app es una constante de tipo obj instanciada
 
-//Enviar una solicitud a traves del metodo GET del HTTP
-//Parametros:
-//Recurso('')=Vacio, el recurso apunta hacia el localhost, puerto 3000
-//Promesa=Una funcion flecha (=>), que costa de:(solicitud, respuesta).
-//=>:Retorna un valor. En este ej. no le indicamos la SOLICITUD, por lo 
-//que toma el acceso al recurso (ruta al localhost). Cuando accede a la
-//ruta, mostramos un msg por consola y enviamos una solicitud 
-app.get('',(req, res) => {
-     console.log('Ejecutando una solicitud GET a la raiz de la App');
-     res.send("Se ingresa al GET de la raiz");
-});
-
-app.get('/Admin',(req, res) => {
-     console.log('App accedida desde el GET');
-     res.send("Se ingresa al GET del recurso Admin");
-});
-
-
+const mongoose = require("mongoose"); //importa el modulo mongoose
+//Conexion hacia la DB (MongoDB Atlas)
+//connect() Cadena de conexion en Atlas(op: Drivers/Connect your application)
+//Replace <password> with NJ-CRUD
+// Entre el / y el simbolo ? colocamos el nombre de la BD (db_crud)
+mongoose.connect(
+ "mongodb+srv://Nj-CRUD:Nj-CRUD@clusternjcrud.rooaifj.mongodb.net/db_crud?retryWrites=true&w=majority&appName=ClusterNjCRUD");
 
 app.listen(3000, ()=>{ //funcion flecha =>, reemplaza la palabra function 
      console.log("Servidor corriendo desde el puerto 3000")
